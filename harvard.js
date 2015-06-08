99ff6ab494f161c5662afc8ab8342a2fd1bc3879
Jay Luker <lbjay@reallywow.com>
2015-06-08 07:54:25 -0400

    Update harvard.js

--- harvard.js
+++ harvard.js
@@ -6,7 +6,7 @@ $(document).ready(function(e) {
     harvardCopy += '<p><a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | <a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Acceptable+Use+Policy+for+Canvas" id="acceptable_use_policy_link">Acceptable Use Policy</a></p>';
     $('footer').html(harvardCopy);
     
-    $(".dce-accordion").accordian();
+    $(".dce-accordion").accordion();
 
 });
 
